4e7bc260a463700ff8d73f98a804f772b79ba170
Atichka <nosik_atichka@mail.ru>
2024-01-24 18:56:42 +0300

    ю

---
+++ src/components/BurgerIngredients/burgerIngredients.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import burgerIngredients from './burgerIngredients.module.css';
+
+class BurgerIngredients extends React.Component {
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default BurgerIngredients;
