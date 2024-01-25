29ae6caec40d0e1728d0e1b46e5156e2accb0378
Atichka <nosik_atichka@mail.ru>
2024-01-26 01:22:46 +0300

    Переписан как функциональный компонент

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -4,21 +4,19 @@ import card from './card.module.css'
 
 import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components'
 
-class Card extends React.Component {
-    render() {
+function Card(props) {
         return (
             <div className={card.card}>
-                <img src={this.props.image} alt='картинка ингредиента' />
+                <img src={props.image} alt='картинка ингредиента' />
                 <div className={card.price}>
-                    <p>{this.props.price}</p>
+                    <p>{props.price}</p>
                     <CurrencyIcon type="primary" />
-                    {this.props.counter > 0 &&
-                    <p className={card.counter}>{this.props.counter}</p>}
+                    {props.counter > 0 &&
+                    <p className={card.counter}>{props.counter}</p>}
                 </div>
-                <p className={card.cardName}>{this.props.name}</p>
+                <p className={card.cardName}>{props.name}</p>
             </div>
         );
-    }
 }
 
 export default Card;
