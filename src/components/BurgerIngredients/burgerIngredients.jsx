aedb052dc2082f3dda6045d6cdeb87a875ec935f
Atichka <nosik_atichka@mail.ru>
2024-01-26 01:10:46 +0300

    Данные созданы с помощью цикла

--- src/components/BurgerIngredients/burgerIngredients.jsx
+++ src/components/BurgerIngredients/burgerIngredients.jsx
@@ -17,6 +17,52 @@ const burgerIngredientsPropTypes = PropTypes.shape({
     price: PropTypes.number.isRequired
 });
 
+const burger = [
+    {
+        id: 1,
+        image: imageBurger1,
+        price: 20,
+        name: "Краторная булка N-200i"
+    },
+    {
+        id: 2,
+        image: imageBurger2,
+        price: 20,
+        name: "Флюоресцентная булка R2-D3"
+    }
+]
+
+const sauce = [
+    {
+        id: 1,
+        image: imageSauce1,
+        price: 30,
+        name: "Соус Spicy-X",
+        counter: 1
+    },
+    {
+        id: 2,
+        image: imageSauce2,
+        price: 30,
+        name: "Соус фирменный Space Sauce",
+        counter: 0
+    },
+    {
+        id: 3,
+        image: imageSauce1,
+        price: 30,
+        name: "Соус Spicy-X",
+        counter: 0
+    },
+    {
+        id: 4,
+        image: imageSauce2,
+        price: 30,
+        name: "Соус фирменный Space Sauce",
+        counter: 0
+    }
+]
+
 class BurgerIngredients extends React.Component {
     render() {
         return (
@@ -35,18 +81,18 @@ class BurgerIngredients extends React.Component {
                     <div className={burgerIngredients.content}>
                         <div>
                             <h2 className={burgerIngredients.text}>Булки</h2>
-                            <div className={burgerIngredients.cards}>
-                                <Card image={imageBurger1} price={"20"} name={"Краторная булка N-200i"} />
-                                <Card image={imageBurger2} price={"20"} name={"Флюоресцентная булка R2-D3"} />
-                            </div>
+                                <div className={burgerIngredients.cards}>
+                                    {burger.map((item, index) => (
+                                        <Card key={item.id} image={item.image} price={item.price} name={item.name} />
+                                    ))}
+                                </div>
                         </div>
                         <div>
                             <h2 className={burgerIngredients.text}>Соусы</h2>
                             <div className={burgerIngredients.cards}>
-                                <Card image={imageSauce1} price={"30"} name={"Соус Spicy-X"} counter={"1"} />
-                                <Card image={imageSauce2} price={"30"} name={"Соус фирменный Space Sauce"} />
-                                <Card image={imageSauce3} price={"30"} name={"Соус Spicy-X"} />
-                                <Card image={imageSauce4} price={"30"} name={"Соус фирменный Space Sauce"} />
+                                {sauce.map((item, index) => (
+                                    <Card key={item.id} image={item.image} price={item.price} name={item.name} counter={item.counter} />
+                                ))}
                             </div>
                         </div>
                     </div>
