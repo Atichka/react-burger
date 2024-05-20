cfc238bd857faf8a30809d4819c236d09a7e5f99
Atichka <nosik_atichka@mail.ru>
2024-05-20 12:13:21 +0300

    Добавлено условие отображения контента

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import {REMOVE_INGREDIENT} from "./services/actions/detailsAction";
 import { OnlyAuth, OnlyUnAuth } from "./components/ProtectedRoute/protectedRoute";
 import {checkUserAuth} from "./services/actions/userAction";
 
+export const ingredients = state => state.ingredients;
 
 export default function App() {
     const [isModal, setModal] = useState(false)
@@ -30,12 +31,12 @@ export default function App() {
     const location = useLocation();
     const navigate = useNavigate();
     const background = location.state && location.state.background;
+    const data = useSelector(ingredients);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getIngredients())
-        dispatch(getConstructor())
     }, [dispatch]);
 
     useEffect(() => {
@@ -49,45 +50,51 @@ export default function App() {
 
   return (
         <div className="App">
-            <AppHeader />
-            <main className={css.block}>
-                <Routes location={background || location}>
-                    <Route path="/login" element={<OnlyUnAuth component={<LoginPage />} />} />
-                    <Route path="/" element={(<MainPage
-                        setModal={setModal}
-                        setWindowIngredient={setWindowIngredient}
-                        setWindowFinish={setWindowFinish} />)} />
-                    <Route path="/register" element={<OnlyUnAuth component={<RegisterPage />} />} />
-                    <Route path="/forgot-password" element={<OnlyUnAuth component={<ForgotPasswordPage />}/>} />
-                    <Route path="/reset-password" element={<OnlyUnAuth component={<ResetPasswordPage />} />
-                    } />
-                    <Route path="/profile" element={<OnlyAuth component={<ProfilePage />} />}>
-                        <Route index element={<OnlyAuth component={<UserProfile />} />}/>
-                        <Route path={'orders'} element={<OnlyAuth component={<div>orders</div>} />} />
-                        <Route path={'orders/:orderNumber'} element={<OnlyAuth component={<div>ordersNumber</div>} />} />
-                    </Route>
-                    <Route path="/ingredients/:id" element={<IngredientDetails />}></Route>
-                </Routes>
-                {background && (
-                    <Routes>
-                        <Route
-                            path="/ingredients/:id"
-                            element={isModal && windowIngredient &&
-                            (<Modal
-                                    setModal={setModal}
-                                    onClose={onClose}
-                                    title='Детали ингредиента'>
-                                <IngredientDetails />
-                            </Modal>)
-                        }></Route>
-                    </Routes>
-                )}
-            </main>
-                {isModal && windowFinish &&
-                    (<Modal setModal={setModal} onClose={onClose}>
-                        <OrderDetails />
-                    </Modal>)
-                }
+            {!data.isLoading && data.ingredients.length===0 ? (
+                <h1>Загрузка данных</h1>
+            ) : (
+                <>
+                    <AppHeader />
+                    <main className={css.block}>
+                        <Routes location={background || location}>
+                            <Route path="/login" element={<OnlyUnAuth component={<LoginPage />} />} />
+                            <Route path="/" element={(<MainPage
+                                setModal={setModal}
+                                setWindowIngredient={setWindowIngredient}
+                                setWindowFinish={setWindowFinish} />)} />
+                            <Route path="/register" element={<OnlyUnAuth component={<RegisterPage />} />} />
+                            <Route path="/forgot-password" element={<OnlyUnAuth component={<ForgotPasswordPage />}/>} />
+                            <Route path="/reset-password" element={<OnlyUnAuth component={<ResetPasswordPage />} />
+                            } />
+                            <Route path="/profile" element={<OnlyAuth component={<ProfilePage />} />}>
+                                <Route index element={<OnlyAuth component={<UserProfile />} />}/>
+                                <Route path={'orders'} element={<OnlyAuth component={<div>orders</div>} />} />
+                                <Route path={'orders/:orderNumber'} element={<OnlyAuth component={<div>ordersNumber</div>} />} />
+                            </Route>
+                            <Route path="/ingredients/:id" element={<IngredientDetails />}></Route>
+                        </Routes>
+                        {background && (
+                            <Routes>
+                                <Route
+                                    path="/ingredients/:id"
+                                    element={isModal && windowIngredient &&
+                                        (<Modal
+                                            setModal={setModal}
+                                            onClose={onClose}
+                                            title='Детали ингредиента'>
+                                            <IngredientDetails />
+                                        </Modal>)
+                                    }></Route>
+                            </Routes>
+                        )}
+                    </main>
+                    {isModal && windowFinish &&
+                        (<Modal setModal={setModal} onClose={onClose}>
+                            <OrderDetails />
+                        </Modal>)
+                    }
+                </>
+            )}
         </div>
   );
 }
