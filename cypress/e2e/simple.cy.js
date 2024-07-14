a3fcfd8be43ddd54a47aa9bbc40579927a24e14e
Atichka <nosik_atichka@mail.ru>
2024-07-15 01:49:46 +0300

    Вынесена константа dataCy

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -1,17 +1,17 @@
-import {testUrl} from "../../src/const";
+import {dataCy, testUrl} from "../../src/const";
 describe('page constructor', function() {
     beforeEach('should be available on localhost:3000', function() {
         cy.visit(testUrl);
     });
     it('should drag ingredients', () => {
         cy.contains('p', 'булка').trigger('dragstart').first();
-        cy.get('[data-cy=list]').trigger('drop');
+        cy.get(dataCy).trigger('drop');
         cy.contains('p', 'Соус').trigger('dragstart').first();
-        cy.get('[data-cy=list]').trigger('drop');
+        cy.get(dataCy).trigger('drop');
         cy.contains('p', 'Соус').trigger('dragstart').first();
-        cy.get('[data-cy=list]').trigger('drop');
+        cy.get(dataCy).trigger('drop');
         cy.contains('p', 'Биокотлета').trigger('dragstart').first();
-        cy.get('[data-cy=list]').trigger('drop');
+        cy.get(dataCy).trigger('drop');
     });
     it('open and close modal with ingredient', () => {
         cy.contains('p', 'булка').click()
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = 'https://norma.nomoreparties.space/api';
 export const WS_FEED_URL = "wss://norma.nomoreparties.space/orders/all";
 export const testUrl = 'http://localhost:3000';
+export const dataCy = '[data-cy=list]';
